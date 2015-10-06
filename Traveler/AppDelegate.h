8e540df991f2a13a4ae75ee8f19e023be064ae9c
pvcary <pvcary@gmail.com>
2015-10-06 13:53:16 -0500

    Initial Commit

---
+++ Traveler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Traveler
+//
+//  Created by Stephen Cary on 10/6/15.
+//  Copyright © 2015 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Traveler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Traveler
+//
+//  Created by Stephen Cary on 10/6/15.
+//  Copyright © 2015 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
